4b7693e8e0ce38d8c0bf5abe4b50d74f026fedd6
Andy Mai <andy.mai@ctx.edu>
2019-03-11 19:25:47 -0500

    beats bettermoves 1/100

--- checkers/AIblackMove.java
+++ checkers/AIblackMove.java
@@ -10,6 +10,7 @@
  */
 
 public class AIblackMove {
+    
     public class MovePair{
 	
 	public CheckersMove move;
@@ -36,7 +37,7 @@ public class AIblackMove {
     public AIblackMove(CheckersGame game, CheckersMove moves[]) {
         currentGame = game;
         legalMoves = moves;
-        depth = 6;
+        depth = 5;
     
     }
 
@@ -47,7 +48,7 @@ public class AIblackMove {
         //return legalMoves[0];
         // 2. Pick a random move
         //return legalMoves[currentGame.generator.nextInt(legalMoves.length)];
-        return minimax(currentGame.boardData, depth, -9999, 9999, true).move;
+        return minimax(currentGame.boardData, null, depth, -9999, 9999, true).move;
         
         //Or you can create a copy of the current board like this:
         //CheckersData new_board = new CheckersData(currentGame.boardData);
@@ -58,81 +59,93 @@ public class AIblackMove {
         //currently legal moves using a loop and select the best one.
     }
     
-    public MovePair minimax(CheckersData oldboard, int depth, int alpha, int beta, boolean maximizingPlayer){
+    public MovePair minimax(CheckersData oldboard, CheckersMove bestMove, int depth, int alpha, int beta, boolean maximizingPlayer){
    
-    CheckersMove bestMove = null;
+    if(depth <= 0 || oldboard.getLegalMoves(CheckersData.BLACK) == null || oldboard.getLegalMoves(CheckersData.RED) == null ){
+            return new MovePair(bestMove,evaluate(oldboard));
+        }
     //CheckersData new_board = new CheckersData(oldboard);
     
     //Get array of all black moves
     CheckersMove blackMoves[] = oldboard.getLegalMoves(CheckersData.BLACK);
+    //CheckersMove bestMove = blackMoves[0];
     //Get array of all red moves
     CheckersMove redMoves[] = oldboard.getLegalMoves(CheckersData.RED);
-    
-    
-        if(depth == 0 || oldboard.getLegalMoves(CheckersData.BLACK) == null || oldboard.getLegalMoves(CheckersData.RED) == null ){
-            return new MovePair(bestMove,evaluate(oldboard));
-        }
-        int max;
-        int min;
-      
-            if (maximizingPlayer){
             
-            max = Integer.MIN_VALUE;
-            //do all the legal moves, reset the board after every move to try all possible moves
-            //for(int i = 0; i < blackMoves.length; i++){
-            for (CheckersMove blackMove : blackMoves) {
-                
-                CheckersData new_board = new CheckersData(oldboard);
-              
-                //new_board.makeMove(blackMove[i];
-                new_board.makeMove(blackMove);
-//                if(new_board.getLegalJumpsFrom(new_board.pieceAt(blackMove.toRow, blackMove.toCol),
-//                            blackMove.toRow, blackMove.toCol) == null) {
-//                } else {
-//                    //bestMove = blackMove;
-//                    maximizingPlayer = !maximizingPlayer;
+            if (maximizingPlayer){
+             MovePair max = new MovePair(bestMove,Integer.MIN_VALUE);
+            //int max = Integer.MIN_VALUE;
+        //do all the legal moves, reset the board after every move to try all possible moves
+        for(int i = 0; i < blackMoves.length; i++){
+        //for (CheckersMove blackMove : blackMoves) {
+          
+            
+            CheckersData new_board = new CheckersData(oldboard);
+            new_board.makeMove(blackMoves[i]);
+            //new_board.makeMove(blackMove);
+            
+//                if(new_board.getLegalJumpsFrom(new_board.pieceAt(blackMoves[i].toRow, blackMoves[i].toCol),
+//                            blackMoves[i].toRow, blackMoves[i].toCol) != null) { maximizingPlayer = !maximizingPlayer; }
+
+            //private boolean canJump(int player, int r1, int c1, int r2, int c2, int r3, int c3)
+//                for(int i = 0; i < redMoves.length; i++){ //try to check and see if any red can jump after making black move, if red can, remove black move from array?
+//                    if(canJump(redMoves[i],new_board.pieceAt(, i)))
 //                }
-                //evals the board at given position
-                int evaluation = minimax(new_board, depth -1, alpha, beta, false).value;
-                //compares that value to max and if its higher than max then it's the new best move
-                if (evaluation > max) {
-                    max = evaluation;
-                    bestMove = blackMove;
-                }
-                alpha = Math.max(alpha, evaluation);
-                if (beta <= alpha){
-                    break;
-                }
+//evals the board at given position
+
+        MovePair evaluation = null;
+       
+        if (bestMove == null) {
+                        evaluation= minimax(new_board, blackMoves[i], depth -1, alpha, beta, !maximizingPlayer);
+                    }else {
+                        evaluation = minimax(new_board, bestMove, depth -1, alpha, beta, !maximizingPlayer);
+                    }
+
+//        int evaluation = minimax(new_board, depth -1, alpha, beta, !maximizingPlayer).value;
+        
+        //compares that value to max and if its higher than max then it's the new best move
+            if (max.value <= evaluation.value) {
+                max = evaluation;
+                //bestMove = blackMoves[i]; 
             }
-            //System.out.println(max);
-            return new MovePair(bestMove, max);
+            alpha = Math.max(alpha, evaluation.value);
+            if (beta <= alpha){
+                break;
+            }
+        }
+        
+            
+            //return new MovePair(bestMove, max);
+            return max;
         }
         else {
-            min = Integer.MAX_VALUE;
+            MovePair min = new MovePair(bestMove, Integer.MAX_VALUE);
             
             //do all the legal moves, reset the board after every move to try all possible moves
-            //for(int i = 0; i < redMoves.length; i++){
-            for (CheckersMove redMove : redMoves) {
+            for(int i = 0; i < redMoves.length; i++){
+            //for (CheckersMove redMove : redMoves) {
                 CheckersData new_board = new CheckersData(oldboard);
-                new_board.makeMove(redMove);
-//                if(new_board.getLegalJumpsFrom(new_board.pieceAt(redMove.toRow, redMove.toCol),
-//                            redMove.toRow, redMove.toCol) != null)
-//                        maximizingPlayer = !maximizingPlayer;
+                new_board.makeMove(redMoves[i]);
+                
+//                if(new_board.getLegalJumpsFrom(new_board.pieceAt(redMoves[i].toRow, redMoves[i].toCol),
+//                            redMoves[i].toRow, redMoves[i].toCol) != null) { maximizingPlayer = !maximizingPlayer; }
+                
                 //evals the board at given position
-                int evaluation = minimax(new_board, depth -1, alpha, beta, true).value;
+                MovePair evaluation = minimax(new_board, bestMove, depth -1, alpha, beta, !maximizingPlayer);
                 //compares that value to min and if its lower than min then it's the new best move for red
-                if (evaluation < min) {
+                if (min.value >= evaluation.value) {
                     min = evaluation;
-                }
-                beta = Math.min(beta, evaluation);
+                }   
+                beta = Math.min(beta, evaluation.value);
                 if (beta <= alpha){
                     break;
                 }
                 
-            }       
+            }    
+             return min;
         }
             
-            return new MovePair(bestMove, min);
+           
     }
         
   
@@ -144,72 +157,168 @@ public class AIblackMove {
     // Also, are kings more valuable than regular pieces?  How much?
     int evaluate(CheckersData board) { 
         
-    int score = (board.numBlack())
-                + (3*board.numBlackKing())
-                - (board.numRed())
-                - (3*board.numRedKing());
+    int score = 0;
     
             double boardValues[][] = {
-              {4, 0, 4, 0, 4, 0, 4, 0},
-              {0, 2, 0, 2, 0, 2, 0, 4},
-              {4, 0, 3, 0, 4, 0, 1, 0},
-              {0, 2, 0, 4, 0, 3, 0, 4},
-              {4, 0, 3, 0, 4, 0, 2, 0},
-              {0, 1, 0, 3, 0, 3, 0, 4},
-              {4, 0, 2, 0, 2, 0, 1, 0},
+              {6, 0, 6, 0, 6, 0, 6, 0},
+              {0, 3, 0, 3, 0, 3, 0, 6},
+              {6, 0, 3, 0, 3, 0, 4, 0},
+              {0, 2, 0, 4, 0, 3, 0, 6},
+              {6, 0, 2, 0, 3, 0, 1, 0},
+              {0, 2, 0, 3, 0, 3, 0, 6},
+              {6, 0, 3, 0, 3, 0, 3, 0},
               {0, 4, 0, 4, 0, 4, 0, 4}
 
       };
+
+//          double boardValues[][] = {
+//              {4, 0, 4, 0, 4, 0, 4, 0},
+//              {0, 2, 0, 2, 0, 2, 0, 4},
+//              {4, 0, 3, 0, 3, 0, 2, 0},
+//              {0, 2, 0, 4, 0, 3, 0, 4},
+//              {4, 0, 3, 0, 4, 0, 2, 0},
+//              {0, 2, 0, 3, 0, 3, 0, 4},
+//              {4, 0, 2, 0, 2, 0, 2, 0},
+//              {0, 4, 0, 4, 0, 4, 0, 4}
+//
+//      };
             
             double rowValues[][] = {
               {5, 0, 5, 0, 5, 0, 5, 0},
-              {0, 1, 0, 1, 0, 1, 0, 1},
-              {1, 0, 1, 0, 1, 0, 1, 0},
-              {0, 2, 0, 1, 0, 1, 0, 1},
-              {1, 0, 1, 0, 1, 0, 1, 0},
-              {0, 1, 0, 1, 0, 1, 0, 1},
-              {1, 0, 1, 0, 1, 0, 1, 0},
+              {0, 1, 0, 3, 0, 3, 0, 1},
+              {1, 0, 3, 0, 3, 0, 1, 0},
+              {0, 1, 0, 2, 0, 2, 0, 1},
+              {1, 0, 2, 0, 2, 0, 1, 0},
+              {0, 1, 0, 3, 0, 3, 0, 1},
+              {1, 0, 3, 0, 3, 0, 1, 0},
               {0, 5, 0, 5, 0, 5, 0, 5}
+
+      };
+            double endValues[][] = {
+              {10, 0, 10, 0, 10, 0, 10, 0},
+              {0, 10, 0, 10, 0, 10, 0, 10},
+              {10, 0, 20, 0, 20, 0, 10, 0},
+              {0, 20, 0, 20, 0, 20, 0, 10},
+              {10, 0, 20, 0, 20, 0, 10, 0},
+              {0, 20, 0, 20, 0, 20, 0, 10},
+              {10, 0, 10, 0, 10, 0, 10, 0},
+              {0, 10, 0, 10, 0, 10, 0, 10}
 
       };
             double halfValues[][] = {
               {5, 0, 5, 0, 5, 0, 5, 0},
               {0, 5, 0, 5, 0, 5, 0, 5},
-              {5, 0, 5, 0, 5, 0, 5, 0},
-              {0, 5, 0, 5, 0, 5, 0, 5},
-              {5, 0, 5, 0, 5, 0, 5, 0},
-              {0, 5, 0, 5, 0, 5, 0, 5},
+              {5, 0, 6, 0, 6, 0, 5, 0},
+              {0, 5, 0, 6, 0, 6, 0, 5},
+              {5, 0, 6, 0, 6, 0, 5, 0},
+              {0, 5, 0, 6, 0, 6, 0, 5},
               {5, 0, 5, 0, 5, 0, 5, 0},
               {0, 5, 0, 5, 0, 5, 0, 5}
 
       };
-         
-        for(int y = 0; y < 8; y++){
-            for(int x = Math.floorMod(y,2); x < 8; x+=2){ //x starts at 0 when y is even and starts at 1 when y is odd
-                int piece = board.pieceAt(x,y);
+        int total = (board.getNumPieces(CheckersData.RED)) + (board.getNumPieces(CheckersData.BLACK));
+            score += (2*board.numBlack())
+                + (board.numBlackKing())
+                - (2*board.numRed())
+                - (3*board.numRedKing());
+            
+//         for(int col = 0; col < 8; col++){
+//            for(int row = Math.floorMod(col,2); row < 8; row+=2){ 
+//                int piece = board.pieceAt(row,col);
+//                switch(piece){
+//                        case CheckersData.BLACK:
+//                                score += (2*boardValues[row][col]); 
+//                                score += (2*rowValues[row][col]);
+//                                //score += (halfValues[row][col]);
+//                                break;
+//                        case CheckersData.BLACK_KING:
+//                                score += (boardValues[row][col]); 
+//                                score += (rowValues[row][col]);
+//                                //score += (3 * halfValues[row][col]);
+//                                break;
+//                        case CheckersData.RED:
+//                                score -= (2*rowValues[row][col]); 
+//                                score -= (2*boardValues[row][col]);
+//                                //score -= (halfValues[row][col]);
+//                                break;
+//                        case CheckersData.RED_KING:
+//                                score -= (3 * rowValues[row][col]);
+//                                score -= (3 * boardValues[row][col]); 
+//                                //score -= (3 * halfValues[row][col]);
+//                                break;
+//                }
+//            }
+//        }
+
+        if((total) < 8 ){
+            score = 0;
+            score += (3*board.numBlack())
+                + (2*board.numBlackKing())
+                - (4*board.numRed())
+                - (5*board.numRedKing());
+            
+          for(int col = 0; col < 8; col++){
+            for(int row = Math.floorMod(col,2); row < 8; row+=2){ 
+                int piece = board.pieceAt(row,col);
                 switch(piece){
                         case CheckersData.BLACK:
-                                score += (3 * boardValues[x][y]); //3
-                                score += (3 * rowValues[x][y]);
-                                score += (3 * halfValues[x][y]);
+                                score += (3* boardValues[row][col]);
+                                score += (3*endValues[row][col]); 
+
                                 break;
                         case CheckersData.BLACK_KING:
-                                score += (5 * boardValues[x][y]); //5
-                                score += (5 * halfValues[x][y]);
+                                score += (2* boardValues[row][col]);
+                                score += (2*endValues[row][col]); 
+                               // score += (3 * halfValues[row][col]);
                                 break;
                         case CheckersData.RED:
-                                score -= (3 * rowValues[x][y]); //3
-                                score -= (3 * boardValues[x][y]);
-                                score -= (3 * halfValues[0][0]);
+                                score += (4 * boardValues[row][col]);
+                                score -= (4 * endValues[row][col]); 
+                                //score -= (2*endValues[row][col]);
+                                //score -= (halfValues[row][col]);
                                 break;
                         case CheckersData.RED_KING:
-                                score -= (5 * boardValues[x][y]); //5
-                                score -= (5 * halfValues[x][y]);
+                                score += (5* boardValues[row][col]);
+                                score -= (5 * endValues[row][col]); 
+                                //score -= (3 * halfValues[row][col]);
+                                break;
+                }
+            }
+        }
+          System.out.println("end game " + score);
+        } else {
+            score += (2*board.numBlack())
+                + (board.numBlackKing())
+                - (2*board.numRed())
+                - (3*board.numRedKing());
+            
+         for(int col = 0; col < 8; col++){
+            for(int row = Math.floorMod(col,2); row < 8; row+=2){ 
+                int piece = board.pieceAt(row,col);
+                switch(piece){
+                        case CheckersData.BLACK:
+                                score += (2*boardValues[row][col]); 
+                                score += (2*rowValues[row][col]);
+                                score += (halfValues[row][col]);
+                                break;
+                        case CheckersData.BLACK_KING:
+                                score += (boardValues[row][col]); 
+                               score += (3 * halfValues[row][col]);
+                                break;
+                        case CheckersData.RED:
+                                score -= (2*rowValues[row][col]); 
+                                score -= (2*boardValues[row][col]);
+                                score -= (halfValues[row][col]);
+                                break;
+                        case CheckersData.RED_KING:
+                                score -= (3 * boardValues[row][col]); 
+                                score -= (3 * halfValues[row][col]);
                                 break;
                 }
             }
         }
         System.out.println(score);
+        }
 
                    
           
